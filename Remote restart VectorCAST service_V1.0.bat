88b11ea028bd5d4bac3c3dc557dbe14bc8c0f7a0
github-liukai <742425759@qq.com>
2022-04-14 11:16:57 +0800

    VectorCAST远程重启服务脚本

---
+++ Remote restart VectorCAST service_V1.0.bat
@@ -0,0 +1,34 @@
+@echo off
+cls
+echo.
+echo.
+color 4
+echo #########################请选择要执行的操作#########################
+echo --1、输入Y并按回车，执行VectorCAST License Service服务重启服务命令--
+echo -------------2、输入N并按回车，退出服务重启执行窗口-----------------
+echo.
+echo 请选择要执行的操作:
+set /p char=
+
+if "%char%"=="Y" (
+echo.
+net use \\win-server\ipc$ "Password123" /user:"Camel"
+
+SC \\win-server  stop "VectorCAST License Service"
+
+SC \\win-server  start "VectorCAST License Service"
+
+rem net stop "VectorCAST License Service"
+rem net start "VectorCAST License Service"
+echo.
+color 2
+echo "VectorCAST License Service服务重启成功！"
+pause
+)
+
+if "%char%"=="N" (
+echo.
+echo 输入了%char%字符，自动退出服务执行窗口！
+pause
+)
+
